3136323137c476befa6ad2634e4d1875bee5a577
Wagner Bitencourt Alves <wagner.bitencourt@hotmail.com.br>
2019-02-06 15:54:23 -0300

    initial commit - WebApiJwt

---
+++ WebApiJwt/Entities/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiJwt.Entities {
+    public class ApplicationDbContext : IdentityDbContext {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseMySql (GetConnectionString ());
+        }
+
+        private static string GetConnectionString () {
+            const string databaseName = "webapijwt";
+            const string databaseUser = "root";
+            const string databasePass = "18220592";
+
+            return $"Server=localhost;" +
+                $"database={databaseName};" +
+                $"uid={databaseUser};" +
+                $"pwd={databasePass};" +
+                $"pooling=true;" +
+                $"persistsecurityinfo=true;";
+        }
+    }
+}
